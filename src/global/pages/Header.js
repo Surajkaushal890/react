4fab706b0e1a8325240ec1e0083843d217de93e1
Surajkaushal890 <suraj.satlujwebsolution@gmail.com>
2024-04-08 13:37:12 +0530

    08/04/2024 1:37

--- src/global/pages/Header.js
+++ src/global/pages/Header.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Button, Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Header() {
-    return (
-        <>
-            <div className='header'>
-                <div>
-                    <Navbar bg="light" expand="lg">
-                        <Container>
-                            <Navbar.Brand href="#">Navbar</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Button variant="outline-success">Button</Button>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <div className='header'>
+      <div className='p-0 bg-colorNav'>
+        <Navbar expand="lg" className='navbar navbar-expand-lg navbar-light px-3'>
+          <Container fluid>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="navbarNavAltMarkup">
+              <Nav className="ms-auto">
+                <Nav.Link href="#" className="text-white">Home</Nav.Link>
+                <Nav.Link href="#" className="text-white">Services</Nav.Link>
+                <Nav.Link href="#" className="text-white">About Us</Nav.Link>
+                <Nav.Link href="#" className="text-white">Blog</Nav.Link>
+                <Nav.Link href="#" className="text-white">Contact Us</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import Header from '../../global/pages/Header'
 import Footer from '../../global/pages/Footer'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function HomePageMain() {
   return (
     <>
       <Header />
-        
+
+      {/* Contact number section */}
+      <Container fluid className="bg-contactnumb pt-2">
+        <Row>
+          <Col xs={12} md={{ span: 4, offset: 4 }} className="treeContent text-center">
+            <h3 className="text-white">City Tree Service</h3>
+            <h2 className="phoneNumb">(415) 000 000</h2>
+            <div className="headBorder"></div>
+          </Col>
+        </Row>
+      </Container>
+      {/* Contact number section close */}
+
+      {/* Banner section */}
+      <section className="bg-hero py-5">
+        <Container>
+          <Row>
+            <Col md={7}>
+              <div className="top-text">
+                <p className="text-white">
+                  <span className="text-warning">&starf; &starf; &starf; &bigstar; &star; </span> 
+                  Rated4.6/5.0 by 238 Happy Customers
+                </p>
+                <h3 className="text-white">Value Proposnon Featuring Benefit </h3>
+                <p className="heroText text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                  <div className="iconBox">
+                    <p className="text-white mb-0"><img src={`${process.env.PUBLIC_URL}/img/checked.png`} alt="Check"></img>Text ever since the 1500s,</p>
+                    <p className="text-white mb-0"><img src={`${process.env.PUBLIC_URL}/img/checked.png`} alt="Check"></img>Text ever since the 1500s,</p>
+                    <p className="text-white  mb-0"><img src={`${process.env.PUBLIC_URL}/img/checked.png`} alt="Check"></img>Text ever since the 1500s,</p>
+                  </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* End Banner section */}
+
       <Footer />
     </>
   );
