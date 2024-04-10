9d66102cada26c4a8414988b680e38e87cd65af2
Surajkaushal890 <suraj.satlujwebsolution@gmail.com>
2024-04-10 12:29:56 +0530

    12:29

--- src/https.js
+++ src/https.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const http = axios.create({
-    baseURL: "https://citytreebackend.satlujwebsolution.com/",
+    baseURL: "https://citytreebackend.satlujwebsolution.com/api",
     headers: {
         'Content-Type': 'application/json',
     }
